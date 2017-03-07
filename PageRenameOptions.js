a66b92b20934251d3fc982e2b0e32eae142dc7c9
adrianbj <adrianbjones@gmail.com>
2017-03-07 11:28:35 -0800

    Add support for Page Clone module. Also some updates for PW 3.

--- PageRenameOptions.js
+++ PageRenameOptions.js
@@ -10,7 +10,21 @@
 
 $(document).ready(function() {
 
-    var $nameField = $("#Inputfield__pw_page_name");
+    if($('input[name="clone_page_name"]').length) {
+        var $nameField = $("#Inputfield_clone_page_name");
+    }
+    else {
+        var $nameField = $("#Inputfield__pw_page_name");
+    }
+
+    if($('input[name="clone_page_title"]').length) {
+        var $titleId = "Inputfield_clone_page_title";
+    }
+    else {
+        var $titleId = "Inputfield_title";
+    }
+
+    $nameId = $nameField.selector.replace('#', '');
 
     // check if namefield exists, because pages like homepage don't have one
     if(!$nameField.length) return;
@@ -26,13 +40,16 @@ $(document).ready(function() {
     });
 
     var pageAutoRename_titleKeyup = function() {
+
         if(!pageAutoRename_active) return;
         if(typeof initialTitleConverted !== 'undefined' && initialTitleConverted != initialName) return; // don't change name if the name was already different at the start of editing
         if(typeof modifiedName !== 'undefined' && modifiedName != initialName) return;
 
         // var val = $(this).val().substring(0, 128);
         var val = $(this).val(); // @adrian
-        var id = $(this).attr('id').replace(/Inputfield_title_*/, 'Inputfield__pw_page_name');
+        var replace = $titleId+"_*";
+        var re = new RegExp(replace);
+        var id = $(this).attr('id').replace(re, $nameId);
         $nameField = $("#" + id);
         var lastName = $nameField.val();
         if($nameField.size() > 0) $nameField.val(val).trigger('blur');
